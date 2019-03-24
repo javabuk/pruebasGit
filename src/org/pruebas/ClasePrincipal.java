b1c0c72af31d3fdde112bd28f6e4d65311ccde73
javabuk <joprieto@gmail.com>
2019-03-24 15:13:37 +0100

    Correción del Bug

--- src/org/pruebas/ClasePrincipal.java
+++ src/org/pruebas/ClasePrincipal.java
@@ -3,8 +3,8 @@ package org.pruebas;
 public class ClasePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Correccion del bug");
+		
 	}
 
 }
